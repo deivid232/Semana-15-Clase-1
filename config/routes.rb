9c9eb42faab6893cb60c6a2e8d29f144b045fb41
david aguilo <david.aguilo232@gmail.com>
2018-08-07 21:50:34 -0400

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   resources :histories
 
   root "histories#index"
+  get 'users/sign_up', to: "users#new"
+  post 'users', to: "users#create"
+  resources :sessions, only: [:create, :destroy]
+  get 'users/sign_in', to: 'sessions#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
